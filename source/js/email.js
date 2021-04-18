e248d4b5390ab22138f07f8a2cf5e8a1038d570b
Rahul Gupta <52673814+RAHUL1115@users.noreply.github.com>
2021-04-18 21:42:03 +0530

    changes

--- source/js/email.js
+++ source/js/email.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
 function sendEmail(name, email) {
   Email.send({
     Host: "smtp.gmail.com",
-    Username: "rahulgupta.ra.com@gmail.com@gmail.com",
+    Username: "stockdock7@gmail.com",
     Password: "adiwsfptbbdnooik",
     To: email,
     From: "stockdock7@gmail.com",
@@ -22,17 +22,17 @@ function sendEmail(name, email) {
     We appreciate your interest in stock markets and are looking forward to see you in the session.<br><br>
 
     If you haven't yet joined the WhatsApp group do join through the below link for further updates:<br>
-    https://chat.whatsapp.com/HBA4iYh9nQ32zqMbF2mSo1  <br><br>
+    https://chat.whatsapp.com/EhhdVvrvTsGKj9BFJP9Guq  <br><br>
 
     Session Details <br>
-    Date : April 3, 2021 <br>
+    Date : April 10, 2021 <br>
     Time : 3pm to 4pm <br><br>
 
     Zoom link to join the session : <br>
-    https://edflylearn.zoom.us/j/88239948439?pwd=V1loc2hkTGVKeXlPTjVBSnJDNkU1QT09 <br><br>
+    https://edflylearn.zoom.us/j/89044094279?pwd=R2w5eTBmc1VqbTV6U1A3OUgzYWFoQT09 <br><br>
 
-    Meeting ID- 882 3994 8439 <br>
-    Password- 341788 <br><br>
+    Meeting ID- 890 4409 4279 <br>
+    Password- 754749 <br><br>
 
     Thank You,<br>
     Regards,<br>
@@ -48,16 +48,91 @@ function sendEmail(name, email) {
 }
 
 function getEmails() {
-  fetch("https://stockdock.glitch.me/admin/admin123/pass123")
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach((reg, index) => {
-        if (index >= 240 && index < 263) {
-          // sendEmail(reg.name.toString(), reg.email.toString());
-        }
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  data = [
+    {
+      "College / institute": "Manibain Nanavati women's college",
+      "Name": "Pallavi Rambhau Gopale",
+      "Email": "pallavigopale61@gmail.com",
+      "WhatsApp No.": 7304272397,
+      "Course": 2021,
+      "Year": "TY"
+    },
+    {
+      "College / institute": "Maniben Nanavati women's college",
+      "Name": "Shemina Lakhani",
+      "Email": "seminalakhani728@gmail.com",
+      "WhatsApp No.": 7738010781,
+      "Course": "TYBAFI",
+      "Year": "TY"
+    },
+    {
+      "College / institute": "Maniben nanavati woman's college",
+      "Name": "sadhana gupta",
+      "Email": "sadhana9650@gmail.com",
+      "WhatsApp No.": 9082000893,
+      "Course": "Bcom (AFI)",
+      "Year": "TY"
+    },
+    {
+      "College / institute": "Maniben Nanavati Women's College",
+      "Name": "Devyanshi",
+      "Email": "devyanshimakwana83@gmail.com",
+      "WhatsApp No.": 7208595675,
+      "Course": "Fybafi",
+      "Year": "FY"
+    },
+    {
+      "College / institute": "The D. G. Ruparel college",
+      "Name": "Priya Sunil Suryavanshi",
+      "Email": "priyasuryavanshi2003@gmail.com",
+      "WhatsApp No.": 9004535440,
+      "Course": "BMS",
+      "Year": "SY"
+    },
+    {
+      "College / institute": "D.G.Ruparel college of arts science and commerce",
+      "Name": "Priyanka prabhakar patil",
+      "Email": "Priyankapatil2984@gmail.com",
+      "WhatsApp No.": 9967679824,
+      "Course": "BMS",
+      "Year": "SY"
+    },
+    {
+      "College / institute": "D.G Ruparel College",
+      "Name": "Suresh Hanumantha Mhatre",
+      "Email": "mhatresuresh895@gmail.com",
+      "WhatsApp No.": 9136584506,
+      "Course": "Bachelor of Management Studies",
+      "Year": "TY"
+    },
+    {
+      "College / institute": "D.G.Ruparel College",
+      "Name": "Shivani Chitre",
+      "Email": "shivanichitre01@gmail.com",
+      "WhatsApp No.": 9833882651,
+      "Course": "BMS",
+      "Year": "SY"
+    },
+    {
+      "College / institute": "D. G. Ruparel college",
+      "Name": "Anuj",
+      "Email": "anujpawar900@gmail.com",
+      "WhatsApp No.": 8928822703,
+      "Course": "Bms",
+      "Year": "FY"
+    },
+    {
+      "College / institute": "Maniben nanavati womens collage",
+      "Name": "vidhya poojary",
+      "Email": "vidhupoojary007@gmail.com",
+      "WhatsApp No.": 8850154759,
+      "Course": "Bcom.afi",
+      "Year": "SY"
+    }
+  ]
+  data.forEach((reg, index) => {
+    if (index >= 0 && index < 20) {
+      // sendEmail(reg.Name.toString(), reg.Email.toString());
+    }
+  });
 }
